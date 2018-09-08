f8d96c1a435cc3022e57ee590fef09978071da73
louayydrs <louayydrs@users.noreply.github.com>
2018-09-08 17:34:39 +0800

    owncloud

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php 
+$OC_Version = array(10,0,9,5);
+$OC_VersionString = '10.0.9';
+$OC_Edition = '';
+$OC_Channel = 'stable';
+$OC_VersionCanBeUpgradedFrom = [[8,2,11],[9,0,9],[9,1]];
+$OC_Build = '2018-07-16T14:15:59+00:00 d5836485ac9802db343b6c9eed986a24b50eb072';
+$vendor = 'owncloud';
